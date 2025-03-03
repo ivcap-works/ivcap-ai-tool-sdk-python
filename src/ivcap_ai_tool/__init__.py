638a12520240faa226617c03b203557023ad14f4
Max Ott <max.ott@data61.csiro.au>
2025-03-03 18:04:21 +1100

    added builder function to simplify tool definition

--- src/ivcap_ai_tool/__init__.py
+++ src/ivcap_ai_tool/__init__.py
@@ -16,3 +16,7 @@ except Exception:
     __version__ = "unknown" # should only happen when running the local examples
 
 from .tool_definition import ToolDefinition, print_tool_definition, create_tool_definition
+from .server import start_tool_server
+
+from .builder import add_tool_api_route, ToolOptions
+from .executor import ExecutionContext
